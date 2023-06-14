6b66dff7b405cd5c7b3edd04f1c9e419b6b1d02b
truongbx_weupgroup <truongbx@weupgroup.vn>
2023-06-14 17:39:00 +0700

    commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,41 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:vimeo_video_player/vimeo_video_player.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Vimeo video player demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  /// also support url like this: 'www.vimeo.com/70591644', 'vimeo.com/70591644'
-  final String _vimeoVideoUrl = 'https://vimeo.com/70591644';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: VimeoVideoPlayer(
-          url: _vimeoVideoUrl,
-          autoPlay: true,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              VimeoVideoPlayer(
+                url: 'https://vimeo.com/157743578',
+                onFinished: () => onFinishedVimeo(),
+              ),
+              VimeoVideoPlayer(
+                url: 'https://vimeo.com/740663286',
+                onFinished: () => onFinishedVimeo(),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void onFinishedVimeo() {
+    print('onFinishedVimeo');
   }
 }
